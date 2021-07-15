062882d6031db401d0d5e0ae6aea8158694e8455
xtawfnhdx <zhangweilong.001@163.com>
2021-07-15 17:58:06 +0800

    功能：练习获取豆瓣数据

---
+++ PythonInterview/reptile/reptile_01_my.py
@@ -0,0 +1,12 @@
+import os
+
+book_info_list={}
+book_info_one={}
+
+
+FILE_LOCLA_PATH="reptile_01"
+def begins():
+    pass
+if __name__=="__main__":
+    if not os.path.exists(FILE_LOCLA_PATH):
+        os.makedirs(FILE_LOCLA_PATH)
