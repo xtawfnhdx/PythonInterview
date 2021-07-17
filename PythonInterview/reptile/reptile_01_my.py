904b7f42e20ad492209596e973604867a645d67a
xtawfnhdx <zhangweilong.001@163.com>
2021-07-17 16:54:50 +0800

    功能：修复一些评论少的时候爬取失败问题

--- PythonInterview/reptile/reptile_01_my.py
+++ PythonInterview/reptile/reptile_01_my.py
@@ -9,21 +9,28 @@ from bs4 import BeautifulSoup
 
 USER_AGENTS = [
     {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},
-    {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},
+    {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},
     {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'},
     {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0) Gecko/20100101 Firefox/73.0'},
-    {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15'},
+    {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15'},
     {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'},
-    {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'},
-    {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'},
+    {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'},
+    {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'},
     {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0'},
     {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)'},
     {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)'},
     {'User-Agent': 'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)'},
-    {'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12'},
-    {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7'},
+    {
+        'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12'},
+    {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7'},
     {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0'},
-    {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}]
+    {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}]
 
 FILE_LOCLA_PATH = "reptile_01"
 
@@ -74,7 +81,11 @@ def get_book_list_info(bookListUrlByTarget):
     for book in booklist:
         bookDetailUrl = book.find('a', {'class': 'nbg'}).get('href')
         bookInfo = get_book_info(bookDetailUrl)
-        bookInfo["BookComment"] = get_book_comment(bookDetailUrl + 'comments/')
+        # 无评论或者评论人数太少，不爬取数据
+        if int(bookInfo['BookDetails']['ScoreCount']) > 0:
+            bookInfo["BookComment"] = get_book_comment(bookDetailUrl + 'comments/')
+        else:
+            bookInfo["BookComment"] = {}
         bookList.append(bookInfo)
     return bookList
 
@@ -102,12 +113,12 @@ def get_book_info(bookInfoUrl):
             bookInfoSoup.find('div', {'id': 'mainpic'}).find('img').get('src')
         ]
         bookScoreMoel = bookInfoSoup.find('div', {'id': 'interest_sectl'})
-        if '人评价' in bookScoreMoel.text:
-            bookDetailDic['Score'] = bookScoreMoel.find('strong', {'class': 'll rating_num'}).text
-            bookDetailDic['ScoreCount'] = bookScoreMoel.find('span', {'property': 'v:votes'}).text
-        else:
+        if '无人评价' in bookScoreMoel.text or '人数不足' in bookScoreMoel.text:
             bookDetailDic['Score'] = 0
             bookDetailDic['ScoreCount'] = 0
+        else:
+            bookDetailDic['Score'] = bookScoreMoel.find('strong', {'class': 'll rating_num'}).text
+            bookDetailDic['ScoreCount'] = bookScoreMoel.find('span', {'property': 'v:votes'}).text
         bookInfoDic['BookDetails'] = bookDetailDic
         return bookInfoDic
     except Exception as e:
@@ -176,5 +187,5 @@ if __name__ == "__main__":
     if not os.path.exists(FILE_LOCLA_PATH):
         os.makedirs(FILE_LOCLA_PATH)
     # tag = ['小说','历史','科普']
-    tag = ['创新','奇怪']
+    tag = ['创新', '奇怪']
     crawling_data_by_tag(tag, 2, True)
