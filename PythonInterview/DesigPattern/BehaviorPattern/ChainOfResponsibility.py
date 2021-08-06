919e2d31f14b6672bcf3aaf6a411479412cbc73d
xtawfnhdx <zhangweilong.001@163.com>
2021-08-06 15:22:59 +0800

    功能：责任链模式，命令模式，迭代器模式，中介者模式学习

--- PythonInterview/DesigPattern/BehaviorPattern/ChainOfResponsibility.py
+++ PythonInterview/DesigPattern/BehaviorPattern/ChainOfResponsibility.py
@@ -1,3 +1,77 @@
 """
 责任链模式
 """
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+
+
+class Handler(ABC):
+    @abstractmethod
+    def setNext(self, h: Handler) -> Handler:
+        pass
+
+    @abstractmethod
+    def handler(self,request) -> Optional[str]:
+        pass
+
+
+class AbstractHandler(Handler):
+    _next: Handler = None
+
+    def setNext(self, h: Handler) -> Handler:
+        self._next = h
+        return h
+
+    @abstractmethod
+    def handler(self, request: Any) -> Optional[str]:
+        if self._next:
+            return self._next.handler(request)
+        return None
+
+
+class MonkeyHandler(AbstractHandler):
+    def handler(self, request: Any) -> str:
+        if request == "Banana":
+            return f"Monkey:I'll eat the {request}"
+        else:
+            return super().handler(request)
+
+
+class SquirrelHandler(AbstractHandler):
+    def handler(self, request: Any) -> str:
+        if request == "Nut":
+            return f"Squirrel: I'll eat the {request}"
+        else:
+            return super().handler(request)
+
+
+class DogHandler(AbstractHandler):
+    def handler(self, request: Any) -> str:
+        if request == "MeatBall":
+            return f"Dog: I'll eat the {request}"
+        else:
+            return super().handler(request)
+
+
+def client_code(h: Handler) -> None:
+    for food in ["Nut", "Banana", "Cup of coffee"]:
+        print(f"\nClient: Who wants a {food}?")
+        result = h.handler(food)
+        if result:
+            print(f"  {result}", end="")
+        else:
+            print(f"  {food} was left untouched.", end="")
+
+
+if __name__ == '__main__':
+    m = MonkeyHandler()
+    s = SquirrelHandler()
+    d = DogHandler()
+    m.setNext(s).setNext(d)
+    print("Chain: Monkey > Squirrel > Dog")
+    client_code(m)
+    print("\n")
+
+    print("Subchain: Squirrel > Dog")
+    client_code(s)
--- PythonInterview/DesigPattern/BehaviorPattern/Command.py
+++ PythonInterview/DesigPattern/BehaviorPattern/Command.py
@@ -1,3 +1,66 @@
 """
 命令模式
 """
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Comand(ABC):
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+
+class SimpleCommand(Comand):
+    def __init__(self, payload: str) -> None:
+        self._payload = payload
+
+    def execute(self) -> None:
+        print(f"SimpleCommand: See, I can do simple things like printing"
+              f"({self._payload})")
+
+
+class ComplexCommand(Comand):
+    def __init__(self, receiver: Receiver, a: str, b: str) -> None:
+        self._receiver = receiver
+        self._a = a
+        self._b = b
+
+    def execute(self) -> None:
+        print("ComplexCommand: Complex stuff should be done by a receiver object", end="")
+        self._receiver.do_something(self._a)
+        self._receiver.do_something_else(self._b)
+
+
+class Receiver:
+    def do_something(self, a: str) -> None:
+        print(f"\nReceiver: Working on ({a}.)", end="")
+
+    def do_something_else(self, b: str) -> None:
+        print(f"\nReceiver: Also working on ({b}.)", end="")
+
+
+class Invoker:
+    def set_on_startr(self, command: Comand):
+        self._on_start = command
+
+    def set_on_finish(self, command: Comand):
+        self._on_finish = command
+
+    def to_something_important(self) -> None:
+        print("Invoker: Does anybody want something done before I begin?")
+        if isinstance(self._on_start, Comand):
+            self._on_start.execute()
+
+        print("Invoker: ...doing something really important...")
+
+        print("Invoker: Does anybody want something done after I finish?")
+        if isinstance(self._on_finish, Comand):
+            self._on_finish.execute()
+
+if __name__=='__main__':
+    invoker=Invoker()
+    invoker.set_on_startr((SimpleCommand('Sya hi')))
+    receiver=Receiver()
+    invoker.set_on_finish(ComplexCommand(receiver,'send email','save report'))
+    invoker.to_something_important()
